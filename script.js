7338d4f95ea1c5e3944bd4e2a119367e098d49c6
Life Media Tamil <83417989+luhareena@users.noreply.github.com>
2026-06-26 22:53:57 +0530

    Add service worker registration to script.js

--- script.js
+++ script.js
@@ -141,3 +141,8 @@ setInterval(() => {
         now.toLocaleString();
 
 }, 1000);
+// Register Service Worker
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js")
+        .then(() => console.log("Service Worker Registered"));
+}
