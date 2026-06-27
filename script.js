48a89badb8a1e7f3a2552f2bf5fd1ff07d470b6a
Life Media Tamil <83417989+luhareena@users.noreply.github.com>
2026-06-27 14:48:44 +0530

    Add event listener for refresh button and service worker

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function showMessage() {
-    alert("Button வேலை செய்கிறது!");
+document.getElementById("refreshBtn").addEventListener("click", () => {
+
+document.getElementById("news").innerHTML=`
+<h2>Latest Update</h2>
+<p>${new Date().toLocaleString()}</p>
+`;
+
+});
+
+if("serviceWorker" in navigator){
+
+window.addEventListener("load",()=>{
+
+navigator.serviceWorker.register("./sw.js")
+.then(()=>console.log("Service Worker Registered"))
+.catch(err=>console.log(err));
+
+});
+
 }
