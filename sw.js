917920eb1d7903993decf35b4f95abeaf0a7a005
Life Media Tamil <83417989+luhareena@users.noreply.github.com>
2026-06-27 09:54:44 +0530

    Update service worker cache name and add manifest

--- sw.js
+++ sw.js
@@ -1,22 +1,39 @@
-const CACHE_NAME = "lifemedia-v1";
+const CACHE_NAME = "life-media-tamil-v1";
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll([
-                "./",
-                "./index.html",
-                "./style.css",
-                "./script.js"
-            ]);
-        })
-    );
+const FILES_TO_CACHE = [
+  "./",
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-    );
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
