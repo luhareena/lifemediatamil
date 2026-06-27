5588802a01ed130858f9dd0bdcf00f2544661d6d
Life Media Tamil <83417989+luhareena@users.noreply.github.com>
2026-06-27 14:50:46 +0530

    Refactor service worker caching logic and file list

--- sw.js
+++ sw.js
@@ -1,39 +1,57 @@
-const CACHE_NAME = "life-media-tamil-v2";
+const CACHE_NAME = "life-media-v1";
 
-const FILES_TO_CACHE = [
-  "./",
-  "index.html",
-  "style.css",
-  "script.js",
-  "manifest.json"
+const FILES = [
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./manifest.json"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener("install",event=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache=>cache.addAll(FILES))
+
+);
+
+self.skipWaiting();
+
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("activate",event=>{
+
+event.waitUntil(
+
+caches.keys().then(keys=>Promise.all(
+
+keys.map(key=>{
+
+if(key!==CACHE_NAME){
+
+return caches.delete(key);
+
+}
+
+})
+
+))
+
+);
+
+self.clients.claim();
+
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+.then(res=>res||fetch(event.request))
+
+);
+
 });
