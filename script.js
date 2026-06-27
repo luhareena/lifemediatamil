dacfa34e11b078c7d91731350ad0f7965a54ab9f
Life Media Tamil <83417989+luhareena@users.noreply.github.com>
2026-06-27 14:42:22 +0530

    Update alert message and remove service worker code
    Replaced welcome message with button action alert and removed service worker registration code.

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 function showMessage() {
-    alert("Life Media Tamil-க்கு வரவேற்கிறோம்!");
-}
-
-// Register Service Worker
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker
-            .register("sw.js")
-            .then(() => {
-                console.log("Service Worker Registered");
-            })
-            .catch((error) => {
-                console.log("Service Worker Registration Failed:", error);
-            });
-    });
+    alert("Button வேலை செய்கிறது!");
 }
